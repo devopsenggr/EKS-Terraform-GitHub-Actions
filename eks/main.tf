46994c11cde1cccacd4b86ad35d9837de8e92191
devopsenggr <152875568+devopsenggr@users.noreply.github.com>
2024-08-15 15:40:58 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "eks" {
-  source = "../module"
+  source = "../module/eks"
 
   env                   = var.env
   cluster-name          = "${local.env}-${local.org}-${var.cluster-name}"
