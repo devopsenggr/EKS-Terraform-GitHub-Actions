46378c0e15f9686a100565c3305640f226d05bc1
devopsenggr <152875568+devopsenggr@users.noreply.github.com>
2024-08-16 14:54:28 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "my-ews-baket132"
+    bucket         = "3tierproj"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
