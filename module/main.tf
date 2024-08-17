2792e44b13de99516ce478b48294893a9854849f
devopsenggr <152875568+devopsenggr@users.noreply.github.com>
2024-08-17 13:48:37 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,4 +1,15 @@
 provider "aws"{
  region ="us-east-1"
 }
+resource "aws_dynamodb_table" "terraform_lock" {
+ name = "terraform-lock"
+ billing+mode = "PAY_PER_REQUEST"
+hash_key = "LockID"
+
+attribute {
+ name ="LockID"
+type = "s"
+}
+
+}
 
